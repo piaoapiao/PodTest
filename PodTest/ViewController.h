a4a9e07d39dc3e0277472edf38f64e747f7d17c3
guodong <>
2018-08-08 16:37:51 +0800

    create new project

---
+++ PodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodTest
+//
+//  Created by guodong on 2018/8/8.
+//  Copyright © 2018年 maizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PodTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PodTest
+//
+//  Created by guodong on 2018/8/8.
+//  Copyright © 2018年 maizi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
